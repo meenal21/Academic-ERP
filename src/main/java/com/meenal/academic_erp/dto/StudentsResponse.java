9891943084d8f7c849c672fb536784dd731ee1c1
Meenal Jain <mpbjain@gmail.com>
2024-11-23 00:17:42 +0530

    Student Response has been initialized"

--- src/main/java/com/meenal/academic_erp/dto/StudentsResponse.java
+++ src/main/java/com/meenal/academic_erp/dto/StudentsResponse.java
@@ -1,4 +1,32 @@
 package com.meenal.academic_erp.dto;
 
-public class StudentsResponse {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+public record StudentsResponse(
+        @JsonProperty("firstName")
+        String firstName,
+
+        @JsonProperty("lastName")
+        String lastName,
+
+        @JsonProperty("email")
+        String email,
+
+        @JsonProperty("cgpa")
+        double cgpa,
+
+        @JsonProperty("domain")
+        String domain,
+
+        @JsonProperty("photographPath")
+        String photographPath,
+
+        @JsonProperty("totalCredits")
+        double totalCredits,
+
+        @JsonProperty("graduationYear")
+        Date graduationYear
+){
 }
